172269ebb0bd2f399b1b5e1636dfb2b65d00e840
cucannucan <112396381+cucannucan@users.noreply.github.com>
2023-11-29 14:26:14 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from flask_cors import CORS
 import json
 
 mysql = MySQL()
-app = Flask(_name_)
+app = Flask(__name__)
 CORS(app)
 
 # MySQL Instance configurations
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'secret'
 app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = '34.39.15.63'
+app.config['MYSQL_HOST'] = '34.125.170.207'
 mysql.init_app(app)
 
 def execute_query(query):
@@ -39,7 +39,7 @@ def add():
         else:
             return '{"Result": "Error"}'
     except Exception as e:
-        return '{"Result": "Error"}'
+        return '{"Result": "Error", "Message": "' + str(e) + '"}'
 
 @app.route("/update", methods=['PUT'])  # Update Student
 def update():
@@ -47,13 +47,15 @@ def update():
     name = request.json.get('name')
     email = request.json.get('email')
     try:
+
+
         query = '''UPDATE students SET studentName = '{}', email = '{}' WHERE studentID = {} ;'''.format(name, email, id)
         print("Received Update Request. ID:", id, "Name:", name, "Email:", email)
         success = execute_query(query)
         print(success)
         return '{"Result": "Success"}'
     except Exception as e:
-        return '{"Result": "Error"}'
+        return '{"Result": "Error", "Message": "' + str(e) + '"}'
 
 @app.route("/delete", methods=['DELETE'])  # Delete Student
 def delete():
@@ -63,8 +65,9 @@ def delete():
         success = execute_query(query)
         print(success)
         return '{"Result": "Success"}'
+
     except Exception as e:
-        return '{"Result": "Error"}'
+        return '{"Result": "Error", "Message": "' + str(e) + '"}'
 
 
 @app.route("/default")  # Default - Show Data
@@ -88,12 +91,16 @@ def read():
         )
         return ret
     except Exception as e:
-        return '{"Result": "Error"}'
+        return '{"Result": "Error", "Message": "' + str(e) + '"}'
     
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='8080')
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080')
